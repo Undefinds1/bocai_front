33e01690848d2d83325de0f6b1ecaf0e7795cf9f
Liuwa113 <1073104684@qq.com>
2022-12-25 14:05:23 +0800

    理赔规则

--- src/pages/Settlement/index.js
+++ src/pages/Settlement/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './index.module.scss';
 import Apply from './apply';
+import Rule from './rule';
 function Settlement(props) {
   return (
     <div className={styles.settlement}>
       <Apply />
+      <Rule />
     </div>
   );
 }
